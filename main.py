8285f7f68b342e8c5414293fae08fcea2c528e72
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-25 11:55:26 +0800

    Predicting the new result and visualing a SVR

--- main.py
+++ main.py
@@ -5,3 +5,43 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+print(X)
+
+# Merubah format y menjadi vertical dan juga menjadi array 2 dimens
+y = y.reshape(len(y), 1)
+print(y)
+
+# Menerapkan feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+
+sc_y = StandardScaler()
+y = sc_y.fit_transform(y)
+
+# Training the SVR model
+from sklearn.svm import SVR
+regressor = SVR()
+regressor.fit(X, y)
+
+# Predicting new result
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1,1))
+
+# Visualising the SVR result
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1,1)), color='blue')
+plt.title("Truth Or Bluff (SVR)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+# Visualising the SVR Result with hight resolution
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid)).reshape(-1,1)), color='blue')
+plt.title("Truth Or Bluff (SVR)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
